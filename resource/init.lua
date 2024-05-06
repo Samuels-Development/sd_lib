cb05a4bb4f45a535dd7927e785b1577282d7649f
Samuel Nicol <samnicol888@gmail.com>
2024-05-06 22:08:40 +0200

    refactor(resources/init): streamlined framework detection

--- resource/init.lua
+++ resource/init.lua
@@ -1,34 +1,39 @@
--- Framework Initialization Functions
-local function ESXInit()
-    if GetResourceState('es_extended') == 'started' then
-        _G.ESX = exports['es_extended']:getSharedObject()
-        _G.Framework = 'esx'
-    end
-end
+-- Global variable to store framework details
+local frameworkId, frameworkObj
 
-local function QBCoreInit()
-    if GetResourceState('qb-core') == 'started' then
-        _G.QBCore = exports['qb-core']:GetCoreObject()
-        _G.Framework = 'qb'
-    end
-end
+-- Initiliaze a framework and return it's name and object.
+local InitFramework = function(framework)
+    local objectName if framework == 'es_extended' then objectName = 'es_extended' elseif framework == 'qb-core' or framework == 'qbx_core' then objectName = 'qb-core' end
 
--- Detect active framework and initialize it
-local function DetectFramework()
-    local frameworks = {'es_extended', 'qb-core'}
-    for _, fw in ipairs(frameworks) do
-        if GetResourceState(fw) == 'started' then
-            if fw == 'es_extended' then
-                ESXInit()
-                return 'esx'
-            elseif fw == 'qb-core' then
-                QBCoreInit()
-                return 'qb'
+    if GetResourceState(framework) == 'started' and objectName then
+        if objectName == 'es_extended' then
+            return 'esx', exports[objectName]:getSharedObject()
+        elseif objectName == 'qb-core' then
+            if framework == 'qbx_core' then
+                return 'qbx', exports[objectName]:GetCoreObject()
+            else
+                return 'qb', exports[objectName]:GetCoreObject()
             end
         end
     end
-    return nil
+    return nil, nil
 end
+
+-- Function to automatically detect a framework and initialize the 'id' and object of said framework.
+local DetectFramework = function()
+    local frameworks = {'es_extended', 'qb-core', 'qbx_core'}
+    for _, fw in ipairs(frameworks) do
+        local id, obj = InitFramework(fw)
+        if id then
+            return id, obj
+        end
+    end
+    return nil, nil
+end
+
+-- Detect and store the framework at the start
+frameworkId, frameworkObj = DetectFramework()
+if frameworkId == 'qb' then _ENV['QBCore'] = frameworkObj elseif frameworkId == 'esx' then _ENV['ESX'] = frameworkObj end
 
 -- Initialization of 'sd_lib' and context determination
 local sd_lib = 'sd_lib'
