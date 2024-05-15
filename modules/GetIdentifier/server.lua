bfe36c4f5a63e14c52ec9407cc5e9d1bfb2687a9
Samuel Nicol <samnicol888@gmail.com>
2024-05-15 14:10:47 +0200

    fix(getIdentifier): check to see if player is not nil

--- modules/GetIdentifier/server.lua
+++ modules/GetIdentifier/server.lua
@@ -1,26 +1,21 @@
 --- Selects and returns the most appropriate function for getting a player's identifier.
 -- This function checks the active server framework (e.g., ESX, QBCore) to determine the best method for retrieving player identifiers.
--- If no known framework is detected, it defaults to using the native GetPlayerIdentifiers function.
----@return function A function tailored to retrieve a player's identifier using the determined method.
+-- If no known framework is detected, it returns nil.
+---@return function|nil A function tailored to retrieve a player's identifier using the determined method, or nil if no known framework is detected.
 local Identifier = function()
     if Framework == 'esx' then
         -- For ESX, directly return the player's primary identifier.
-        return function(source)
-            local player = SD.GetPlayer(source)
+        return function(player)
             return player.identifier
         end
     elseif Framework == 'qb' or Framework == 'qbx' then
         -- For QBCore, return the player's citizenid.
-        return function(source)
-            local player = SD.GetPlayer(source)
+        return function(player)
             return player.PlayerData.citizenid
         end
     else
-        -- Fallback method using GetPlayerIdentifiers for unsupported frameworks.
-        return function(source)
-            local identifiers = GetPlayerIdentifiers(source)
-            return identifiers[1]
-        end
+        -- Return nil if no known framework is detected.
+        return nil
     end
 end
 
@@ -31,7 +26,8 @@ local GetIdentifier = Identifier()
 -- The method to determine the identifier is set during the initialization of the script.
 -- @param source any The input used to identify and retrieve the player's unique identifier.
 SD.GetIdentifier = function(source)
-    return GetIdentifier(source)
+    local player = SD.GetPlayer(source)
+    if player then return GetIdentifier(player) end
 end
 
 return SD.GetIdentifier
