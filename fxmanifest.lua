86f5904bbc97c0e502461b8402fb8d92e4aaf951
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2024-07-20 10:45:52 +0200

    chore: bump version to 1.0.33

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 Author 'Samuel#0008'
 Description 'A library of functions, that also bridges frameworks and common 3rd party resources.'
-Version '1.0.32'
+Version '1.0.33'
 
 client_scripts {
     'resource/**/client.lua',
