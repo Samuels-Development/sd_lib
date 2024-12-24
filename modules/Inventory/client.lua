0bea6339704981665f4c25f3bdac9b161e7fc988
Hankaws <hankawsproduction@gmail.com>
2024-12-24 16:35:09 +0100

    tgiann-inventory support

--- modules/Inventory/client.lua
+++ modules/Inventory/client.lua
@@ -4,11 +4,14 @@ SD.Inventory = {}
 local inventorySystem
 local codemInv = 'codem-inventory' -- Variable to store the string name codem-inventory
 local oxInv = 'ox_inventory' -- Variable to store the string name ox_inventory
+local tgiannInv = 'tgiann-inventory' -- Variable to store the string name tgiann-inventory
 
 if GetResourceState(codemInv) == 'started' then
     inventorySystem = 'codem'
 elseif GetResourceState(oxInv) == 'started' then
     inventorySystem = 'ox'
+elseif GetResourceState(tgiannInv) == 'started' then
+    inventorySystem = 'tgiann'
 end
 
 -- Dynamic selection function to determine how to check for item existence.
@@ -54,6 +57,42 @@ local HasItem = function()
                 return result
             else
                 return exports[oxInv]:Search('count', items)
+            end
+        end
+    elseif inventorySystem == 'tgiann' then
+        return function(items)
+            local playerInventory = exports[tgiannInv]:GetPlayerItems()
+            local result = {}
+        
+            if type(items) == 'table' then
+                -- Initialize result table with 0 for each item in `items`
+                for k in pairs(items) do
+                    result[k] = 0
+                end
+                
+                -- Iterate through the player's inventory and count matching items
+                for _, itemData in pairs(playerInventory or {}) do
+                    local itemName = tostring(itemData.name or "")
+                    if items[itemName] then
+                        -- Use `count` if available, otherwise fall back to `amount`
+                        local itemAmount = itemData.count or itemData.amount or 0
+                        result[itemName] = result[itemName] + itemAmount
+                    end
+                end
+                
+                return result
+            else
+                -- Single item check
+                local itemCount = 0
+                for _, itemData in pairs(playerInventory or {}) do
+                    if tostring(itemData.name or "") == tostring(items) then
+                        -- Use `count` if available, otherwise fall back to `amount`
+                        local itemAmount = itemData.count or itemData.amount or 0
+                        itemCount = itemCount + itemAmount
+                    end
+                end
+                
+                return itemCount
             end
         end
     elseif Framework == 'esx' then
