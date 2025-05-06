6fdf5f0b33988dd9d1152cce8a95fb5597384dc2
Snowly <visionhyronic@gmail.com>
2025-05-06 13:54:57 +0200

    fix(names): Use coroutine instead of Await as it is not implemented yet (me sad rn)

--- modules/Name/client.lua
+++ modules/Name/client.lua
@@ -1,18 +1,39 @@
 SD.Name = {}
 
 SD.Name.GetFullName = function()
-    local fullName = SD.Callback.Await('sd_lib:getFullName')
-    return(fullName)
+    local thread = coroutine.running()
+    assert(thread, "SD.Name.GetFullName must be called from within a coroutine")
+
+    SD.Callback.Register("sd_lib:getFullName", function(source)
+        local result = SD.Name.GetFullName(source)
+        coroutine.resume(thread, result)
+    end)
+
+    return coroutine.yield()
 end
 
 SD.Name.GetFirstName = function()
-    local firstName = SD.Callback.Await('sd_lib:getFirstName')
-    return(firstName)
+    local thread = coroutine.running()
+    assert(thread, "SD.Name.GetFirstName must be called from within a coroutine")
+
+    SD.Callback.Register("sd_lib:getFirstName", function(source)
+        local result = SD.Name.GetFirstName(source)
+        coroutine.resume(thread, result)
+    end)
+
+    return coroutine.yield()
 end
 
 SD.Name.GetLastName = function()
-    local lastName = SD.Callback.Await('sd_lib:getLastName')
-    return(lastName)
+    local thread = coroutine.running()
+    assert(thread, "SD.Name.GetLastName must be called from within a coroutine")
+
+    SD.Callback.Register("sd_lib:getLastName", function(source)
+        local result = SD.Name.GetLastName(source)
+        coroutine.resume(thread, result)
+    end)
+
+    return coroutine.yield()
 end
 
 return SD.Name
